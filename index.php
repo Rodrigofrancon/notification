683a08ceecd4e6445f5e138bafd5704675982900
Rodrigo Franco <rodrigofranco@MacBook-Air-de-Rodrigo.local>
2020-08-05 22:57:09 -0300

    Commit Initial

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/lib_ext/autoload.php';
+
+use Notification\Email;
+
+$novoEmail = new Email;
+$novoEmail->sendMail("", "<p>Esse e um email de <b>teste</b>b></p>p>", "", "", "", "");
+
+var_dump($novoEmail);
