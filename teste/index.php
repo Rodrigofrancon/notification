d6180b57c0d92e87b3f3c4305846d93a9a81a450
Rodrigo Franco <rodrigofranco@MacBook-Air-de-Rodrigo.local>
2020-08-06 16:31:48 -0300

    Atualizacao Versao

--- teste/index.php
+++ teste/index.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email;
-$novoEmail->sendMail("", "<p>Esse e um email de <b>teste</b>b></p>p>", "", "", "", "");
+$novoEmail = new Email(2,"mail.host.com", "your@email.com", "your-pass", "smtp secure(tls / ssl", "port(587 / 465", "from@email.com", "from Name");
+$novoEmail->sendMail("Subject", "Content", "replay@email.com", "Replay Name", "address@email.com", "Address Name");
 
 var_dump($novoEmail);
